d75e4f118d9eb6384a7e0bba18a73d6a010a77f4
bryansegovia <bsegovia@espol.edu.ec>
2025-04-13 19:13:05 -0500

    Aquí inicia mi camino ninja en Flutter

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://onaplus.delo.si/media/images/20250130/1939996.2e16d0ba.fill-800x450.jpg',
+            ),
+          ),
+        ),
+        title: const Text('Mi amooor ❤️'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 50,
+                itemBuilder: (context, index) {
+                  return MyMessageBubble();
+                },
+              ),
+            ),
+            const Text('Mundo'),
+          ],
+        ),
+      ),
+    );
+  }
+}
