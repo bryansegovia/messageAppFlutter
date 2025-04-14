bb334c2ecb2f0500c4806e25fa4694bb9aa3442c
bryansegovia <bsegovia@espol.edu.ec>
2025-04-13 23:20:42 -0500

    feat: Sigo aprendiendo, ahora sí a mimir

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+    final colors = Theme.of(context).colorScheme;
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inputDecoration = InputDecoration(
+      hintText: 'Finaliza tu mensaje con "?"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () {
+          final texValue = textController.value.text;
+          print('button: $texValue');
+          textController.clear();
+        },
+        icon: const Icon(Icons.send_outlined),
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Usando onFieldSubmitted $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
